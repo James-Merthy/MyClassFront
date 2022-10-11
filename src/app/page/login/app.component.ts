1f2a7f49372e39c9c98deffbc48a6c0bace84430
MyClass <anasala.merthy@gmail.com>
2022-10-11 16:03:41 +0200

    page admin

---
+++ src/app/page/login/app.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {SessionService} from "../../module/security/services/session.service";
+import {AuthService} from "../../module/security/services/auth.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'MyClassFront';
+  username: string = "";
+  password: string = "";
+
+  // getters
+  get isConnected(): boolean {
+    return this._session.isConnected();
+  }
+
+  constructor(private _session: SessionService, private _auth: AuthService, private router: Router) {}
+
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: any) {
+    console.log("CHANGES");
+  }
+
+  onSubmit(): void {
+    this._auth.login(this.username, this.password).subscribe(data => {
+      console.log(data["token"]);
+      let token = data["token"];
+      this._session.login(token);
+      this.router.navigate(["/"]);
+    });
+  }
+}
--- src/app/page/login/app.module.ts
+++ src/app/page/login/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {SecurityModule} from "../../module/security/security.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SecurityModule
   ],
   providers: [],
+  exports: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
